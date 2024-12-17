0e19c90ea7b916eb5427c306d131203062f7b93a
leewanjae <dhkswo1@gmail.com>
2024-12-17 15:40:27 +0900

    대표님 Sample 구조 Scanning 후 City 3D Model 랜더링

---
+++ ARTestCode/ViewModel/ARMainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ARMainViewModel.swift
+//  ARTestCode
+//
+//  Created by LeeWanJae on 12/17/24.
+//
+
+import Foundation
+import ARKit
+import RealityKit
+
+final class ARMainViewModel {
+    
+}
