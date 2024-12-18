a20bc463df3ae7a940098fd14d53f7df340761d5
leewanjae <dhkswo1@gmail.com>
2024-12-18 13:58:32 +0900

    vm으로 로직 이동

---
+++ ARTestCode/Presentation/View/ARMainViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ARMainViewController.swift
+//  ARTestCode
+//
+//  Created by LeeWanJae on 12/16/24.
+//
+
+import ARKit
+import RealityKit
+import UIKit
+
+final class ARMainViewController: UIViewController {
+    // MARK: - Properties
+    var referenceARObjectName: UILabel
+    var restartButton: UIButton
+    var arView: ARView
+    
+    private var viewModel: ARMainViewModel
+    
+    // MARK: - Initialize
+    init() {
+        self.referenceARObjectName = UILabel()
+        self.restartButton = UIButton()
+        self.arView = ARView(frame: .zero)
+        self.viewModel = ARMainViewModel()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        addView()
+        setAutoLayout()
+        arView.session.delegate = self
+        viewModel.setupARSession(arView: arView)
+    }
+    
+    // MARK: - UI
+    private func addView() {
+        view.addSubview(arView)
+        view.addSubview(referenceARObjectName)
+        view.addSubview(restartButton)
+    }
+    
+    private func setUI() {
+        arView.frame = view.bounds
+        
+        referenceARObjectName.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        referenceARObjectName.textColor = .white
+        referenceARObjectName.translatesAutoresizingMaskIntoConstraints = false
+        referenceARObjectName.text = "Loading..."
+        
+        restartButton.setTitle("restart", for: .normal)
+        restartButton.setTitleColor(.white, for: .normal)
+        restartButton.translatesAutoresizingMaskIntoConstraints = false
+        restartButton.addTarget(self, action: #selector(restartARSession), for: .touchUpInside)
+    }
+    
+    private func setAutoLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            referenceARObjectName.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
+            referenceARObjectName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            
+            restartButton.topAnchor.constraint(equalTo: referenceARObjectName.bottomAnchor, constant: 10),
+            restartButton.leadingAnchor.constraint(equalTo: referenceARObjectName.leadingAnchor)
+        ])
+    }
+    
+    @objc private func restartARSession() {
+        viewModel.restartARSession(for: arView)
+        referenceARObjectName.text = "Loading..."
+    }
+}
+
+// MARK: Delegate
+extension ARMainViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let objectAnchor = anchor as? ARObjectAnchor {
+                let position = objectAnchor.transform.columns.3
+                
+                let xFormatted = String(format: "%.2f", position.x)
+                let yFormatted = String(format: "%.2f", position.y)
+                let zFormatted = String(format: "%.2f", position.z)
+                
+                referenceARObjectName.text = "object: \(objectAnchor.referenceObject.name ?? "N/A"), x: \(xFormatted), y: \(yFormatted), z: \(zFormatted)"
+                
+                viewModel.addARContent(objectAnchor: objectAnchor, arView: arView)
+            }
+        }
+    }
+}
---
+++ ARTestCode/Presentation/ViewModel/ARMainViewModel.swift
@@ -0,0 +1,91 @@
+//
+//  ARMainViewModel.swift
+//  ARTestCode
+//
+//  Created by LeeWanJae on 12/17/24.
+//
+
+import ARKit
+import RealityKit
+
+final class ARMainViewModel {
+    // MARK: - Properties
+    private var processedAnchors = Set<UUID>()
+    private var processedRenderedObjects = Set<String>()
+
+    // MARK: - AR
+    func setupARSession(arView: ARView) {
+        let configuration = ARWorldTrackingConfiguration()
+        
+        if let referenceObjects = ARReferenceObject.referenceObjects(inGroupNamed: "AR Resources", bundle: nil) {
+            configuration.detectionObjects = referenceObjects
+        }
+        
+        arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func restartARSession(for arView: ARView) {
+        let configuration = ARWorldTrackingConfiguration()
+        
+        if let referenceObjects = ARReferenceObject.referenceObjects(inGroupNamed: "AR Resources", bundle: nil) {
+            configuration.detectionObjects = referenceObjects
+        }
+        
+        arView.scene.anchors.removeAll()
+        
+        processedAnchors.removeAll()
+        processedRenderedObjects.removeAll()
+        
+        arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        print("AR 세션 재시작")
+    }
+    
+    func addARContent(objectAnchor: ARObjectAnchor, arView: ARView) {
+        guard !processedAnchors.contains(objectAnchor.identifier) else {
+            print("Anchor ID \(objectAnchor.identifier) 이미 처리되었습니다. 렌더링을 건너뜁니다.")
+            return
+        }
+        processedAnchors.insert(objectAnchor.identifier)
+        
+        guard let objectName = objectAnchor.referenceObject.name else {
+            print("Anchor \(objectAnchor.identifier)의 referenceObject에 이름이 없습니다. 렌더링 불가.")
+            return
+        }
+        
+        guard !processedRenderedObjects.contains(objectName) else {
+            print("객체 \(objectName)은 이미 렌더링되었습니다. 중복 렌더링을 건너뜁니다.")
+            return
+        }
+        processedRenderedObjects.insert(objectName)
+        
+        let arObjectAnchor = AnchorEntity(anchor: objectAnchor)
+        
+        guard let city = load3DModel(source: "City") else { return print("City 모델이 없습니다.")}
+        guard let car = load3DModel(source: "Car") else { return print("Car 모델이 없습니다.") }
+        
+        city.position = SIMD3(0, 0, 0)
+        city.generateCollisionShapes(recursive: true)
+        
+        car.position = SIMD3(city.position.x - 150, city.position.y + 30, city.position.z)
+        car.scale = SIMD3(0.5, 0.5, 0.5)
+        car.generateCollisionShapes(recursive: true)
+
+        city.addChild(car)
+        arObjectAnchor.addChild(city)
+        
+        arView.installGestures([.all], for: city)
+        arView.installGestures([.all], for: car)
+
+        arView.scene.addAnchor(arObjectAnchor)
+    }
+    
+    private func load3DModel(source: String) -> ModelEntity? {
+        do {
+            let modelEntity = try ModelEntity.loadModel(named: source)
+            return modelEntity
+        } catch {
+            print("\(source) 불러오기 실패")
+            return nil
+        }
+    }
+}
